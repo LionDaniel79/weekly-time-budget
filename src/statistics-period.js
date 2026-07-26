9c5a856ec3c3e2ea0f1b3dd1887956c74182e464
HyunSic <ditto0310@gmail.com>
2026-07-26 09:24:44 +0900

    refactor: remove obsolete month period helper

--- src/statistics-period.js
+++
@@ -1,7 +0,0 @@
-export function visibleComparisonMonthCount(year, referenceDate = new Date()) {
-  const selectedYear = Number(year);
-  const currentYear = referenceDate.getFullYear();
-  if (selectedYear < currentYear) return 12;
-  if (selectedYear > currentYear) return 0;
-  return referenceDate.getMonth() + 1;
-}
